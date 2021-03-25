fac905a142f352d40d2db91d398ef57f258620e0
joely <joely_w@outlook.com>
2021-03-25 15:55:58 +0000

    Created manager.ts

---
+++ src/services/manager.ts
@@ -0,0 +1,32 @@
+import Game from './game';
+
+
+export default class GameManager {
+    public games: { [gameID: string]: Game };
+
+    constructor(){
+        this.games = {};
+    }
+    /**
+     * Generates a random ID for game
+     * @returns {string} Unique random ID
+     */
+    private generateID(): string {
+        const id = Math.random().toString(36).substring(7);
+        if (id in this.games) {
+            return this.generateID();
+        } else {
+            return id;
+        }
+    }
+
+    /**
+     * Add a new game object to the list of games
+     * @returns {string} The ID of the game
+     */
+    public generateGame(): string {
+        const id = this.generateID();
+        this.games[id] = new Game(id);
+        return id;
+    }
+}
