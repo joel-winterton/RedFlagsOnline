b4d6e2257c02732d23a99b815cfcdee09a4bd94b
joely <joely_w@outlook.com>
2021-03-25 15:53:33 +0000

    Added loaders

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 const express = require('express');
-
 import config from './config';
-
+import {createServer} from 'http';
 async function startServer() {
     const app = express();
-    await require('./loaders').default({expressApp: app});
-    app.listen(config.port, () => {
+    const http = createServer(app);
+    await require('./loaders').default({expressApp: app, httpServer: http });
+    http.listen(config.port, () => {
         console.log(`Server listening at: http://localhost:${config.port}`)
     })
 }
